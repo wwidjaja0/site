f07c2fc8229cc4ab9ad79c47b0e7210c0f84b8b0
William Widjaja <contact@wwidjaja.com>
2025-09-10 13:00:01 -0700

    Update from wwidjaja.dev to wwidjaja.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wwidjaja.dev',
+  site: 'https://wwidjaja.com',
   integrations: [mdx(), sitemap()],
   adapter: netlify(),
 });
